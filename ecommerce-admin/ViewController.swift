6866462cd9a32ebede94b29823a5649f20d1c44e
prodev880110 <jack.dev880110@gmail.com>
2020-11-23 14:23:17 +0200

    initial commit

---
+++ ecommerce-admin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ecommerce-admin
+//
+//  Created by Avi Aminov on 23/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
